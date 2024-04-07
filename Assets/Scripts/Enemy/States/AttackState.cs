d9bcef9913fb9847dc6d1add780c7db84c7892eb
Samo Bartolj <samo.bartolj@gmail.com>
2024-04-07 21:09:33 +0200

    Dodan Glavni meni

--- Assets/Scripts/Enemy/States/AttackState.cs
+++ Assets/Scripts/Enemy/States/AttackState.cs
@@ -11,11 +11,6 @@ public class AttackState : State
 
     public override State Tick(EnemyManager enemyManager, EnemyStats enemyStats, EnemyAnimatorManager enemyAnimatorManager)
     {
-        // Select one of out many attacks based on attack scores
-        // if the selected attack is not being able to used because of bad angle or distance, select new attack
-        //if the attack is viable, stop our movement and attack our target
-        //set our recovery timer to the attacks recovery time
-        //return the combat stance state
 
         Vector3 targetDierciton = enemyManager.currentTarget.transform.position - transform.position;
         float viewableAngle = Vector3.Angle(targetDierciton, transform.forward);
